7a6de1e328acbbf04669c3487bbfcdae3e808285
er0evelina <34801600+er0evelina@users.noreply.github.com>
2025-12-04 18:46:55 +0300

    Обновление app.py

--- task3/app.py
+++ task3/app.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template
-from config import *
-from models.repositories import TeacherRepJson
+from flask import Flask, request
+from config import REPO_TYPES, DEFAULT_REPO_TYPE
+from controllers.create_repo import CreateRepoFactory
+from controllers.controllers import TeacherController
+from views.views import TeacherView
 
 app = Flask(__name__)
 
-# Создаем репозиторий
-repo = TeacherRepJson(JSON_FILE_PATH)
+controller = TeacherController(None)
+view = TeacherView()
+
+controller.attach(view)
 
 
 @app.route('/')
 def teachers_list():
-    try:
-        all_teachers = repo.get_k_n_short_list(repo.get_count(), 1)
-    except IndexError:
-        all_teachers = []
+    repo_type = request.args.get('repo_type', DEFAULT_REPO_TYPE)
 
-    return render_template('teachers.html', teachers=all_teachers)
+    repository = CreateRepoFactory.create_repo(repo_type)
+
+    controller.repository = repository
+    controller.load_teachers(repo_type)
+
+    return view.render()
 
 
 if __name__ == '__main__':
