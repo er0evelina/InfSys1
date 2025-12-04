565e587cd9c6e8f797475b28e44c7a4d506d3cd8
er0evelina <34801600+er0evelina@users.noreply.github.com>
2025-12-04 18:32:39 +0300

    Update app.py. Вывод учителей на странице

--- task3/app.py
+++ task3/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from config import *
+from models.repositories import TeacherRepJson
 
 app = Flask(__name__)
 
+# Создаем репозиторий
+repo = TeacherRepJson(JSON_FILE_PATH)
+
+
 @app.route('/')
-def home():
-    return 'Hello, world!'
+def teachers_list():
+    try:
+        all_teachers = repo.get_k_n_short_list(repo.get_count(), 1)
+    except IndexError:
+        all_teachers = []
+
+    return render_template('teachers.html', teachers=all_teachers)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
